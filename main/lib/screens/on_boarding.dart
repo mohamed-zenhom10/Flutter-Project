2e11e7093def756b283a596fa676d4fcca02fff1
Mohamed Zenhom <mzenhom763@gmail.com>
2026-03-24 18:47:03 +0200

    first commit

---
+++ main/lib/screens/on_boarding.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+
+import 'LoginScreen.dart';
+
+class OnBoarding extends StatefulWidget {
+  const OnBoarding({super.key});
+
+  @override
+  State<OnBoarding> createState() => _OnBoardingState();
+}
+
+class _OnBoardingState extends State<OnBoarding> {
+
+  final PageController _controller = PageController();
+  int currentPage = 0;
+
+  final List<String> images = [
+    "assets/main_splash_1.png",
+    "assets/main_splash_2.png",
+    "assets/main_splash_3.png",
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+
+    Size size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      body: Column(
+        children: [
+          Expanded(
+            child: PageView.builder(
+              controller: _controller,
+              itemCount: images.length,
+              onPageChanged: (index) {
+                setState(() {
+                  currentPage = index;
+                });
+              },
+              itemBuilder: (context, index) {
+                return Center(
+                  child: Image.asset(
+                    images[index],
+                    width: size.width * 0.8,
+                  ),
+                );
+              },
+            ),
+          ),
+
+          /// 🟣 dots indicator
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: List.generate(images.length, (index) {
+              return Container(
+                margin: const EdgeInsets.all(5),
+                width: currentPage == index ? 12 : 8,
+                height: currentPage == index ? 12 : 8,
+                decoration: BoxDecoration(
+                  color: currentPage == index ? Colors.blue : Colors.grey,
+                  shape: BoxShape.circle,
+                ),
+              );
+            }),
+          ),
+
+          const SizedBox(height: 20),
+
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                minimumSize: Size(double.infinity, 50),
+              ),
+              onPressed: () {
+                if (currentPage == images.length - 1) {
+
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                      builder: (_) => const LoginScreen(),
+                    ),
+                  );
+                } else {
+                  _controller.nextPage(
+                    duration: const Duration(milliseconds: 300),
+                    curve: Curves.easeIn,
+                  );
+                }
+              },
+              child: Text(
+                currentPage == images.length - 1 ? "Get Start" : "Next",
+              ),
+            ),
+          ),
+
+          const SizedBox(height: 40),
+        ],
+      ),
+    );
+  }
+}
---
+++ main/lib/screens/splash_screen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
+import 'package:main/screens/on_boarding.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+
+    Size size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      body: Center(child: Image.asset("assets/app_icon.png" , width: size.width * 0.5,).animate(
+        onComplete: (v) => {
+          Navigator.push(context , CupertinoPageRoute(builder: (context) => OnBoarding())),
+        }
+      ).fadeIn(
+        duration: Duration(milliseconds: 500),
+      ).fadeOut(
+        delay: Duration(seconds: 2),
+        duration: Duration(milliseconds: 500),
+      ),
+      )
+    );
+  }
+}
